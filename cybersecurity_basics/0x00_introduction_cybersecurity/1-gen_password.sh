24c9eabaf867eccf2043c6404f2880000f651f9e
chloe0524 <chloco31@gmail.com>
2024-07-29 14:22:43 +0200

    changed placement of the cat -n to earlier in the sequence

--- cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -n $(< /dev/urandom tr -dc '[:alnum:]' | head -c $1) | fold -w $1 | sed 's/$/\n/'
+< /dev/urandom tr -dc '[:alnum:]' | cat -n | head -c "$1" |  fold -w "$1" | sed 's/^ *[0-9]* *//' 
