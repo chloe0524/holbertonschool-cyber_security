4269eadd6c4a5fc75f97a84592bd131665946357
chloe0524 <chloco31@gmail.com>
2024-07-29 19:12:26 +0200

    Create a Bash script that monitors all processes started by specified user.

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-IFS=$'\n' 
-for line in $(ps -aux | grep "^$1 ")
-do
-    vsz=$(echo $line | awk '{print $5}')
-    rss=$(echo $line | awk '{print $6}')
-    if [[ "$vsz" -gt 0 && "$rss" -gt 0 ]]
-    then 
-        echo $line
-    fi
-done
+IFS=$'\n'; for line in $(ps -aux | grep "^$1 "); do     vsz=$(echo $line | awk '{print $5}');     rss=$(echo $line | awk '{print $6}');     if [[ "$vsz" -gt 0 && "$rss" -gt 0 ]];     then          echo $line;     fi; done
