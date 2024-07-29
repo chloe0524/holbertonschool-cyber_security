f86a78ff52e80e8ea9a1a31a03c92f9c86b6dcc0
chloe0524 <chloco31@gmail.com>
2024-07-29 19:20:05 +0200

    changed appraoch to use awk

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=$'\n'; for line in $(ps -aux | grep "^$1 "); do     vsz=$(echo $line | awk '{print $5}');     rss=$(echo $line | awk '{print $6}');     if [[ "$vsz" -gt 0 && "$rss" -gt 0 ]];     then          echo $line;     fi; done
+ps -aux | awk '/^'$1' /{if ($5 > 0 && $6 > 0) print $0}'
