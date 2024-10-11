6082a86a9f1a1766aa711ac15a51c13373675fa6
chloe0524 <chloco31@gmail.com>
2024-10-11 14:24:32 +0200

    handled null byte with _

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sliced_password=${1#"{xor}"};
-decoded_base64=$(echo "$sliced_password" | base64 --decode);
+sliced_password=${1#"{xor}"}
+decoded_base64=$(echo "$sliced_password" | base64 --decode | xxd -p -c 256)
 decoded_password=""
-for ((i=0; i<${#decoded_base64}; i++)); do
-	char=${decoded_base64:i:1}
-	ascii_value=$(printf "%d" "'$char'")
-	xor_value=$((ascii_value ^ 95))
-	xor_char=$(printf "\\$(printf '%03o' "$xor_value")")
-	decoded_password+="$xor_char"
+
+for ((i=0; i<${#decoded_base64}; i+=2)); do
+    hex_char=${decoded_base64:i:2}
+    ascii_value=$((16#$hex_char))
+    xor_value=$((ascii_value ^ 95))
+    xor_char=$(printf "\\$(printf '%03o' "$xor_value")")
+    decoded_password+="$xor_char"
 done
-echo "$decoded_password"
+
+echo -e "$decoded_password"
