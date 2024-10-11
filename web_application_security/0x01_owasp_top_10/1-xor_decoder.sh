d89d97fb8944c8a580bd4dd499e2f6d468695401
chloe0524 <chloco31@gmail.com>
2024-10-11 11:49:36 +0200

    test

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-sliced_password=${1#"{xor}"}
-decoded_base64=$(echo "$sliced_password" | base64 --decode | tr -d '\0' | tr -d '\n')  # Remove null byte and newline
-decoded_password=""
-for ((i=0; i<${#decoded_base64}; i++)); do
-    char=${decoded_base64:i:1}
-    ascii_value=$(printf "%d" "'$char'")
-    xor_value=$((ascii_value ^ 95))
-    xor_char=$(printf "\\$(printf '%03o' "$xor_value")")
-    decoded_password+="$xor_char"
+
+password="$1"
+
+password="${password#'{xor}'}"
+
+decoded_password=$(echo -n "$password" | openssl enc -base64 -d)
+
+output=""
+
+for ((i = 0; i < ${#decoded_password}; i++)); do
+    char="${decoded_password:$i:1}"
+    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
+    output+=$(printf "\\$(printf '%03o' $xor_result)")
 done
-echo -n "$decoded_password"
+
+echo "$output"
