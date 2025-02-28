f80bcf09e10a838ebeb00dae670901301aadb69d
chloe0524 <chloco31@gmail.com>
2025-02-28 11:09:12 +0100

    7th task

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap --scanflags ACK -p $2 --exclude-ports $3 $1 -oN custom_scan.txt > /dev/null 2>&1
+sudo nmap --scanflags ACK -p $2 --exclude-ports $3 $1 -oN custom_scan.txt >/dev/null 2>&1
